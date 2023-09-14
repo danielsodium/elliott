a3d5bd2e9feed407f85ac45f8a478c5edce7ad96
Daniel <element11sodium@gmail.com>
2023-09-14 17:59:04 -0400

    status

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const fs = require('node:fs');
 const path = require('node:path');
-const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { Client, Collection, Events, GatewayIntentBits, ActivityType } = require('discord.js');
 const { token } = require('./config.json');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
@@ -24,7 +24,8 @@ for (const folder of commandFolders) {
 }
 
 client.once(Events.ClientReady, () => {
-	console.log('Ready!');
+	console.log('Game Start!');
+	client.user.setActivity('the holy spirit', { type: ActivityType.Listening });
 });
 
 client.on(Events.InteractionCreate, async interaction => {
